8a547319bbae1c03f3acad305e7fd140c62a654e
volkovasystems <richeve.bebedor@gmail.com>
2017-07-13 01:06:48 +0800

    Save changes.

---
+++ .install.sh
@@ -0,0 +1,16 @@
+yarn add \
+babel-runtime \
+harden \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+harden \
+--save \
+--force-latest
